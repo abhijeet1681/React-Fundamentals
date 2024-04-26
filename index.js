ee787a59828e7978561e48cf2a1d8f18acd4b52b
abhijeet1681 <abhijeet.1083846@ratnamcollege.edu>
2024-04-26 21:33:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// const root=document.createElement("div");
+// root.id="root";
+
+// const heading=document.createElement("h1");
+// heading.className="heading";
+// heading.innerHTML="Hello World";
+
+// root.appendChild(heading);
+
+// document.body.appendChild(root);
+
+// console.log("React",React);
+//  console.log("React",ReactDOM);
